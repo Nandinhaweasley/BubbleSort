70b2eb135deb00a19bf937c46ad1d35f30de6951
Nanda Rocha <105882361+Nandinhaweasley@users.noreply.github.com>
2025-10-01 22:03:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BubbleSort;
+string[] arrNumbers = new string[] {
+    "Nanda", "Erick", "Tcholinha", "Amor"
+};
+
+foreach (var number in arrNumbers)
+    Console.Write($"[{number}]");
+    
+var arrOrdered = BubbleSortOrder.Sort<string>(arrNumbers);
+Console.WriteLine("");
+Console.WriteLine("vetor Ordenado Decrescente:");
+foreach (var number in arrOrdered)
+    Console.Write($"[{number}]"); 
